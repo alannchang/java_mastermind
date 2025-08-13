a39652ca8e37a34a69bd1be71c7e9a7e17c9eab2
Alan Chang <alannnchang@gmail.com>
2025-08-13 16:04:44 -0700

    added GameConfigDTO to store user adjustable game settings

---
+++ src/main/java/org/alanc/mastermind/config/GameConfigDTO.java
@@ -0,0 +1,23 @@
+package org.alanc.mastermind.config;
+
+public class GameConfigDTO {
+    private int maxAttempts;
+    private int codeLength;
+    private int maxNumber;
+
+    private GameConfigDTO (int maxAttempts, int codeLength, int maxNumber) {
+        this.maxAttempts = maxAttempts;
+        this.codeLength = codeLength;
+        this.maxNumber = maxNumber;
+    }
+
+    // Getters
+    public int getMaxAttempts() {return maxAttempts;}
+    public int getCodeLength() {return codeLength;}
+    public int getMaxNumber() {return maxNumber;}
+
+    // Setters
+    public void setMaxAttempts(int maxAttempts) {this.maxAttempts = maxAttempts;}
+    public void setCodeLength(int codeLength) {this.codeLength = codeLength;}
+    public void setMaxNumber(int maxNumber) {this.maxNumber = maxNumber;}
+}
